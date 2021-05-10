e617c258a27ac490f5146d09541a46dfd1233cf9
atai <soorbekovataj@gmail.com>
2021-05-10 20:46:19 +0600

    add registration

--- product/models.py
+++ product/models.py
@@ -10,20 +10,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save()
+    # ----------------------------------------------
+    #       автослаг
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.title)
+    #     super().save()
 
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, primary_key=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
 
     class Meta:
         ordering = ['-pub_date']
@@ -31,9 +34,11 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            current = timezone.now().strftime('%s')
-            self.slug = slugify(self.title) + current
-        super().save()
+    # ----------------------------------------------
+    #       автослаг
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         current = timezone.now().strftime('%s')
+    #         self.slug = slugify(self.title) + current
+    #     super().save()
 
